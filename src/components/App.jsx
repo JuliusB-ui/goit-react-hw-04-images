cf504cc53e69a181d9e4ced5b73cd97327eb7efd
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-07-06 21:55:30 +0800

    done
    for submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,153 +1,92 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { getAPI } from '../pixabay-api.js';
 import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
-import { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import PropTypes from 'prop-types';
 
-// const App = () => {
-//   const [images, setImages] = useState([]);
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [isLoading, setisLoading] = useState(false);
-//   const [isError, setisError] = useState(false);
-//   const [isEnd, setIsEnd] = useState(false);
 
-//   useEffect(() = {
-//     const fetchImages = async () => {
-//       if (!searchQuery) return;
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setisLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
 
-//       setisLoading(true);
-//       setisError(false);
+  useEffect(() => {
+    const fetchImages = async () => {
+      // if no search input, return
+      if(!searchQuery) return;
+      
+      setisLoading(true);
+      setisError(false);
 
-//       try {
-//         const response = await getAPI(searchQuery, currentPage);
-//         const { totalHits, hits } = response;
+      try {
+        const response = await getAPI(searchQuery, currentPage);
+        const { totalHits, hits } = response;
+        
+        setImages(currentPage===1 ? hits : [...images, ...hits]);
+        setIsEnd(images.length+hits.length >= totalHits);
 
-//         this.setState(prevState => ({
-//           images: currentPage === 1 ? hits : [...prevState.images, ...hits],
-//           isLoading: false,
-//           isEnd: prevState.images.length + hits.length >= totalHits,
-//         }));
-
-//         if (hits.length === 0) {
-//           alert('No images found. Try a different search.');
-//           return;
-//         }
-//       } catch (error) {
-//         this.setState({ isLoading: false, isError: true });
-//         alert(`An error occurred while fetching data: ${error}`);
-//       }
-//     }
-//   }, [])
-
-//   return (
-//     <div>
-//         <SearchBar onSubmit={handleSearchSubmit} />
-//         <ImageGallery images={images} />
-//         {isLoading && <Loader />}
-//         {!isLoading && !isError && images.length > 0 && !isEnd && (
-//           <Button onClick={handleLoadMore} />
-//         )}
-//         {isError && <p>Something went wrong. Please try again later.</p>}
-//       </div>
-//   )
-// };
-
-export class App extends Component {
-  state = {
-    images: [],
-    currentPage: 1,
-    searchQuery: '',
-    isLoading: false,
-    isError: false,
-    isEnd: false,
-  };
-
-  async componentDidUpdate(_prevProps, prevState) {
-    const { searchQuery, currentPage } = this.state;
-
-    if (
-      prevState.searchQuery !== searchQuery ||
-      prevState.currentPage !== currentPage
-    ) {
-      await this.fetchImages();
-    }
-  }
-
-  fetchImages = async () => {
-    this.setState({ isLoading: true, isError: false });
-
-    const { searchQuery, currentPage } = this.state;
-
-    try {
-      const response = await getAPI(searchQuery, currentPage);
-      const { totalHits, hits } = response;
-
-      this.setState(prevState => ({
-        images: currentPage === 1 ? hits : [...prevState.images, ...hits],
-        isLoading: false,
-        isEnd: prevState.images.length + hits.length >= totalHits,
-      }));
-
-      if (hits.length === 0) {
-        alert('No images found. Try a different search.');
-        return;
+        if(hits.length===0) {
+          toast.error("No images found. Try a different search.");
+          return;
+        }
+        
+      } catch (error) {
+        setisError(true);
+        toast.error(`An error occurred while fetching data: ${error}`)
       }
-    } catch (error) {
-      this.setState({ isLoading: false, isError: true });
-      alert(`An error occurred while fetching data: ${error}`);
+      finally {
+        setisLoading(false)
+      }
     }
-  };
 
-  handleSearchSubmit = query => {
+    fetchImages();
+  },[searchQuery,currentPage])
+  
+  const handleSearchSubmit =(query) => {
     const normalizedQuery = query.trim().toLowerCase();
-    const normalizedCurrentQuery = this.state.searchQuery.toLowerCase();
-
-    if (normalizedQuery === '') {
-      alert(`Empty string is not a valid search query. Please type again.`);
+    const normalizedCurrentQuery = searchQuery.toLowerCase();
+    if(normalizedQuery==='') {
+      toast(`Empty string is not a valid search query. Please type again.`)
       return;
     }
-
     if (normalizedQuery === normalizedCurrentQuery) {
-      alert(
+      toast(
         `Search query is the same as the previous one. Please provide a new search query.`
       );
       return;
     }
-
-    // Only update the state and fetch images if the new query is different
-    if (normalizedQuery !== normalizedCurrentQuery) {
-      this.setState({
-        searchQuery: normalizedQuery,
-        currentPage: 1,
-        images: [],
-        isEnd: false,
-      });
+    if(normalizedQuery!==normalizedCurrentQuery) {
+      setSearchQuery(normalizedQuery);
+      setCurrentPage(1);
+      setImages([]);
+      setIsEnd(false);
     }
-  };
-
-  handleLoadMore = () => {
-    if (!this.state.isEnd) {
-      this.setState(prevState => ({ currentPage: prevState.currentPage + 1 }));
-    } else {
-      alert("You've reached the end of the search results.");
-    }
-  };
-
-  render() {
-    const { images, isLoading, isError, isEnd } = this.state;
-    return (
-      <div>
-        <SearchBar onSubmit={this.handleSearchSubmit} />
-        <ImageGallery images={images} />
-        {isLoading && <Loader />}
-        {!isLoading && !isError && images.length > 0 && !isEnd && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-        {isError && <p>Something went wrong. Please try again later.</p>}
-      </div>
-    );
   }
+    
+  const handleLoadMore = () => {
+    if (!isEnd) {
+      setCurrentPage(currentPage+1);
+    } else {
+      toast("You've reached the end of the search results.");
+    }
+  }
+
+  return (
+    <div>
+      <Toaster/>
+      <SearchBar onSubmit={handleSearchSubmit} />
+      <ImageGallery images={images} />
+      {isLoading && <Loader />}
+      {!isLoading && !isError && images.length > 0 && !isEnd && (
+        <Button onClick={handleLoadMore} />
+      )}
+      {isError && <p>Something went wrong. Please try again later.</p>}
+    </div>
+  );
 }
